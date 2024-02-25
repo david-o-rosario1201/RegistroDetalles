e048681209c8869dda5f4c6ee49e362c5d4b0b1a
david-o-rosario1201 <finnnn023@gmail.com>
2024-02-25 18:16:25 -0400

    Creé la libreria con el modelo Prioridades y también el Contexto.

---
+++ RegistroDetalles.Api/DAL/Contexto.cs
@@ -0,0 +1,14 @@
+﻿using Library.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace RegistroDetalles.Api.DAL;
+
+public class Contexto : DbContext
+{
+    public Contexto(DbContextOptions<Contexto> options) : base(options)
+    {
+        
+    }
+
+    public DbSet<Prioridades> Prioridades { get; set; }
+}
