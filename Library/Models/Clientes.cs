d1b00b7aac604ae47c5e34bb7350092bb6716523
david-o-rosario1201 <finnnn023@gmail.com>
2024-02-25 18:33:23 -0400

    Hicé el modelo Clientews y borré la migración porque de problemas agregarlas una a una.

---
+++ Library/Models/Clientes.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Models;
+
+public class Clientes
+{
+	[Key]
+	public int ClienteId { get; set; }
+
+	[Required(ErrorMessage = "Error, debe ingresar un nombre")]
+	public string? Nombre { get; set; }
+
+	[Phone(ErrorMessage = "Error, el formato del número de teléfono no es válido")]
+	[Required(ErrorMessage = "Error, debe ingresar un número de teléfono")]
+	public string? Telefono { get; set; }
+
+	[Phone(ErrorMessage = "Error, el formato del número de celular no es válido")]
+	[Required(ErrorMessage = "Error, debe ingresar un número de celular")]
+	public string? Celular { get; set; }
+
+	[Required(ErrorMessage = "Error, debe ingresar un RNC")]
+	[StringLength(11, MinimumLength = 11, ErrorMessage = "El RNC debe tener exactamente 11 caracteres")]
+	public string RNC { get; set; }
+
+	[EmailAddress(ErrorMessage = "Error, el formato del email no es válido")]
+	[Required(ErrorMessage = "Error, debe un ingresar un email")]
+	public string? Email { get; set; }
+
+	[Required(ErrorMessage = "Error, debe una dirección")]
+	public string? Direccion { get; set; }
+}
