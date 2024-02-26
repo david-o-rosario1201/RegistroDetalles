7f9c2cd108a27adf6e12a8d6c3350bfd8444671a
david-o-rosario1201 <finnnn023@gmail.com>
2024-02-26 00:09:53 -0400

    Hubo un problema con las migraciones, por lo que tuve que hacer algunos ajustes.

--- RegistroDetalles.Api/Migrations/20240226025836_Inicial2.Designer.cs
+++ RegistroDetalles.Api/Migrations/20240226025836_Inicial2.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RegistroDetalles.Api.DAL;
 
@@ -10,9 +11,11 @@ using RegistroDetalles.Api.DAL;
 namespace RegistroDetalles.Api.Migrations
 {
     [DbContext(typeof(Contexto))]
-    partial class ContextoModelSnapshot : ModelSnapshot
+    [Migration("20240226025836_Inicial2")]
+    partial class Inicial2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.2");
